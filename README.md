18ef101fb7e6a6a8d119722c34c4834fc0e5aaac
Marcos Vinicius <119542231+marcos-vinicius14@users.noreply.github.com>
2023-07-11 22:43:40 +0000

    add prettier formater

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto proposto do Curso.dev do Filipe Deschamps, onde criaremos uma aplicação real do Tab News fora a fora
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você consegue. Siga em frente, mesmo que seja sozinho!</h1>
+  return <h1>Você consegue. Siga em frente, mesmo que seja sozinho!</h1>;
 }
 
 export default Home;
