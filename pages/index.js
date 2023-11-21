343271724ff429c3790864c23b4de115bed13f20
Marcos Vinicius <119542231+marcos-vinicius14@users.noreply.github.com>
2023-11-21 11:57:41 +0000

    add services scripts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import database from 'infra/database'
+import database from '/infra/database'
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum");
