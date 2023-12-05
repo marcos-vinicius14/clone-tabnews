1af7fadbcdfa4b3da81e55c6acce87821fec63ad
Marcos Vinicius <119542231+marcos-vinicius14@users.noreply.github.com>
2023-12-05 07:07:04 +0000

    Add test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-    response.status(200).json({chave: "valor"})
+import database from 'infra/database.js'
+
+async function status(request, response) {
+    const result = await database.query("SELECT 1 + 1 as sum")
+    console.log(result.rows);
+    response.status(200).json({ chave: "São acima da média"})
 }
 
 export default status;
